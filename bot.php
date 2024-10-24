e62de4d4ccd28e6f1f316d969b48cb884ea45e15
telegrambots768 <telegrambots768@gmail.com>
2024-10-25 01:45:23 +0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7875381421:AAHWEkD0V8B4rBVIDd4GJ6Dh1H6a5Jm5Mdw";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
